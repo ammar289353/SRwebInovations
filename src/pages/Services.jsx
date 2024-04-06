e6a79b6ae29363471f2b472ecae06109a4ade913
maryam-fati <112760984+maryam-fati@users.noreply.github.com>
2024-04-05 19:51:03 -0700

    servicepage almost done

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -86,10 +86,10 @@ const Services = () => {
       </div>
       <div className="px-12 py-2 pt-6 grid lg:grid-cols-3 gap-8">
         {servicesData.map((service, index) => (
-          <div key={index} className="shadow-2xl shadow-slate-400 pb-4 lg:h-96 mb-5">
+          <div key={index} className="shadow-2xl text-black hover:text-white bg-[#fff] hover:bg-[#093E86] shadow-slate-400 pb-4 lg:h-96 mb-5">
             <img className="pt-12 px-36 w-[380px]" src={service.imageUrl} alt="" />
             <div>
-              <h1 className="text-center font-bold text-[4vw] lg:text-[2vw] pt-3 ">
+              <h1 className=" text-center font-bold text-[4vw] lg:text-[2vw] pt-3 ">
                 {service.title}
               </h1>
               <p className="text-[2vw] pt-3 lg:text-[1vw] px-12 ">
@@ -97,7 +97,7 @@ const Services = () => {
               </p>
             </div>
             <div className="text-center pt-6">
-              <button className="border-[2px] border-blue-800 rounded-full bg-[#ffff] text-black px-5 py-1 border-rounded-lg ">
+              <button className=" hover:text-white border-[2px] border-blue-800 hover:border-white rounded-full bg-[#ffff] hover:bg-[#093E86] text-black px-5 py-1 border-rounded-lg ">
                 Learn More
               </button>
             </div>
@@ -106,10 +106,54 @@ const Services = () => {
       </div>
     </section>
     {/* OUR WORK PORTFOLIO */}
-    <section>
-     
+    <section className='bg-[#F0F6FB] w-full h-[60vw]'>
+      <div className='flex w-[92vw] m-auto'> 
+        <div className='w-[50vw] mt-16'>
+          <p className='font-semibold text-[#05185A]'>OUR WORK PORTFOLIO</p>
+          <h1 className='text-4xl font-bold pt-3 text-[#05185A]'>Explore Our Portfolio That Showcases Our Expertise And Creativity</h1>
+          <p className='text-[1vw] pt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+          <button className='bg-blue-700 border rounded-full p-2 text-white pl-7 pr-7 mt-10'>Explore More</button>
+          <div className='flex mt-8'>
+          <div className='mr-4'>
+          <img src="https://techigems.pk/wp-content/uploads/2024/02/Rectangle-63.png" alt="" />
+          </div>
+          <div>
+            <img src="https://techigems.pk/wp-content/uploads/2024/02/Rectangle-64.png" alt="" />
+          </div>
+          </div>
+        </div>
+        <div className='w-[50vw] mt-16 ml-7'>
+          <img src="https://techigems.pk/wp-content/uploads/2024/02/services.png" alt="" />
+        </div>
+      </div>
     </section>
-        </>
+    {/* OUR EXPERIENCE */}
+     <section>
+      <div className='w-[93vw] m-auto'>
+        <p className='font-semibold text-center mt-10'>OUR EXPERIENCE</p>
+        <h1 className='font-bold text-center text-[3vw]'>We Have Been Working In <br /> The Industry Since 2016</h1>
+        <p className='text-center text-[1vw] mt-3'>We have established ourselves as a leading provider of innovative technology solutions. <br />With our state-of-the-art services, we can help you take your business to the next level and beyond.</p>
+        <div className='flex mt-10 mb-10'>
+          <div className='rounded-xl border border-gray w-[22vw] h-[13vw] mr-4'>
+            <h1 className='text-center text-black font-bold text-[3.5vw] mt-3'>14k</h1>
+            <p className='text-center text-[1.5vw] text-[#7A5353]'>Projects <br /> Done</p>
+          </div>
+          <div className='rounded-xl border bg-blue-700 w-[22vw] h-[13vw] mr-4'>
+            <h1 className='text-center text-white font-bold text-[3.5vw] mt-3'>12k+</h1>
+            <p className='text-center text-white text-[1.5vw]'>Happy <br />Customers</p>
+          </div>
+          <div className='rounded-xl border border-gray w-[22vw] h-[13vw] mr-4'>
+            <h1 className='text-center text-black font-bold text-[3.5vw] mt-3'>4.7</h1>
+            <p className='text-center text-[1.5vw] text-[#7A5353]'>Customer <br /> Review</p>
+          </div>
+          <div className='rounded-xl border bg-blue-700 w-[22vw] h-[13vw] '>
+            <h1 className='text-center text-white font-bold text-[3.5vw] mt-3'>15+</h1>
+            <p className='text-center text-[1.5vw] text-white'>Years <br />Experience</p>
+          </div>
+        </div>
+      </div>
+     </section>
+    </>
     );
 };
 
