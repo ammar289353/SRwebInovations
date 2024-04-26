4937be7f39f557535e5aeb7e5a84e56f4b25a182
maryam-fati <112760984+maryam-fati@users.noreply.github.com>
2024-04-25 19:22:14 -0700

    res

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -52,8 +52,8 @@ const Contact = () => {
                 </div>
             </section>
                 {/* <h1 className='text-[5vw] font-bold text-[#545353] text-center'>Contact Us</h1> */}
-            <section className='w-[90vw] m-auto'>
-                <div className='flex pt-6'>
+            <section className='w-[90vw] m-auto mt-6'>
+                <div className='block sm:block md:flex lg:flex pt-6'>
                 <div className="shadow-2xl rounded-lg shadow-slate-300 pb-4 lg:m-5 lg:w-[30vw] lg:h-72 mb-5">
                     <img className="pt-12 px-36 w-[380px]" src="https://techigems.pk/wp-content/uploads/2024/02/Frame.png" alt="" />
                     <h1 className="text-center font-semibold text-[4vw] lg:text-[1.6vw] pt-3 text-gray-600 ">
@@ -89,9 +89,9 @@ const Contact = () => {
                 </div>
                 </div>
             </section>
-        <section className='w-[93vw] m-auto'>
-            <div className='flex'>
-            <div className='w-[30vw] h-[38vw] mt-16'>
+        <section className='w-[93vw] m-auto mt-10 pb-10'>
+            <div className='block sm:block md:flex lg:flex'>
+            <div className='w-[30vw] h-auto sm:h-auto md:h-[38vw] lg:h-[38vw] mt-16'>
              <p className='text-blue-800 font-semibold'>CONTACT US</p>
              <h1 className='text-[3vw] font-bold'>Get in touch</h1>
              <div className='pb-10 border-b'>
@@ -111,19 +111,27 @@ const Contact = () => {
             <div className="shadow-2xl rounded-lg shadow-slate-300 pb-4 lg:m-5 lg:w-[70vw] lg:h-[45vw] mb-5">
               <h1 className='text-[2.7vw] text-gray-800 font-bold pt-7 pl-10'>Send a message</h1>
               <p className='text-[1.2vw] pl-10 text-gray-500 '>We have established ourselves as a leading provider of innovative <br /> technology solutions.</p>
-              <form action="" className='grid grid-cols-2 pt-5'>
-                <input type="text" name='name' placeholder='Name' className='bg-gray-100 rounded-full w-[25vw] p-[1vw] ml-10 '/>
-                <input type="text" name='company' placeholder='Company' className='bg-gray-100 rounded-full w-[25vw] p-[1vw]'/>
-                <input type="phone" name='compant' placeholder='Phone'  className='bg-gray-100 rounded-full w-[25vw]  mt-3 p-[1vw] ml-10 '/>
-                <input type="email" name='email' placeholder='Email' className='bg-gray-100 rounded-full w-[25vw] mt-3  p-[1vw]'/>
+              <form action="" className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 pt-5'>
+                <input type="text" name='name' placeholder='Name' className='bg-gray-100 rounded-full w-[65vw] sm:w-[70vw] md:w-[25vw] lg:w-[25vw] p-[1vw] ml-10 '/>
+                <input type="text" name='company' placeholder='Company' className='bg-gray-100 rounded-full w-[65vw] sm:w-[70vw] md:w-[25vw] lg:w-[25vw]  p-[1vw] ml-10 sm:ml-10 mt-3 sm:mt-3'/>
+                <input type="phone" name='compant' placeholder='Phone'  className='bg-gray-100 rounded-full w-[65vw] sm:w-[70vw] md:w-[25vw] lg:w-[25vw]  mt-3 p-[1vw] ml-10 '/>
+                <input type="email" name='email' placeholder='Email' className='bg-gray-100 rounded-full w-[65vw] sm:w-[70vw] md:w-[25vw] lg:w-[25vw] mt-3  p-[1vw] ml-10 sm:ml-10'/>
               </form>
-              <input type="text" name='subject' placeholder='Subject' className='bg-gray-100 rounded-full w-[52vw] mt-3  p-[1vw] ml-10'/>
-              <textarea name="message" type='text' placeholder='Message' className='bg-gray-100 rounded-lg mt-3  w-[52vw] p-[1vw] ml-10 pl-6'></textarea>
+              <input type="text" name='subject' placeholder='Subject' className='bg-gray-100 rounded-full  w-[65vw] sm:w-[70vw] md:w-[52vw] lg:w-[52vw] mt-3  p-[1vw] ml-10'/>
+              <textarea name="message" type='text' placeholder='Message' className='bg-gray-100 rounded-lg mt-3  w-[65vw] sm:w-[70vw] md:w-[52vw] lg:w-[52vw] p-[1vw] ml-10 pl-6'></textarea>
             <button className='bg-blue-700 w-[50vw] rounded-full ml-14 text-white p-3 mt-2'>
                 Send
             </button>
             </div>
             </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23078.288084510416!2d74.28194354170905!3d31.462312357965956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391903e4b7e6ff87%3A0xcc62842150b2ec7!2sBlock-C%2C%20faisal%20Town!5e1!3m2!1sen!2s!4v1698343462968!5m2!1sen!2s" 
+           width="100%"
+              height="270" 
+              title='map'
+              style={{border:0, marginLeft: "10px", borderRadius: "0px"}}
+              allowFullScreen="" 
+              loading="lazy" 
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
         </section>
     </>
   )
